41cba2e0dc9e584a78492875e4aca83878474bfc
Igor_gorlov <gorlov35@gmail.com>
2015-10-01 01:23:02 +0300

    ...

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -1,24 +1,11 @@
 import gulp        from 'gulp';
 import runSequence from 'run-sequence';
-<<<<<<< HEAD
 import ghpages     from 'gulp-gh-pages';
 
 gulp.task('deploy', () => (
 	runSequence(
 		'del',
 		'build',
-		() => gulp.src('dist/**/*').pipe(ghpages())
+		() => gulp.src('dist/**/*').pipe(ghpages({branch: 'dist'}))
 	)
-=======
-import ftp         from 'gulp-deploy-ftp';
-
-gulp.task('deploy', () => (
-		() => gulp.src('dist/**/*').pipe(ftp({
-			host: '31.170.165.194',
-			user: 'u178938601',
-			password: '24B0kB3F4hW5',
-			uploadPath: 'public_html/masterica'
-
-		}))
->>>>>>> add responsive
 ));
