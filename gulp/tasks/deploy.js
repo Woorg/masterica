7f4ea86484ed6be97139044b7ad7fe58e2f8a394
Leonard Schuetz <leni.schuetz@me.com>
2015-12-01 21:34:54 +0100

    Removed confidental information
    You shouldn't write out your login informations like that..

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -4,9 +4,9 @@ import ftp         from 'gulp-deploy-ftp';
 
 gulp.task('deploy', () => (
 		() => gulp.src('dist/**/*').pipe(ftp({
-			host: '31.170.165.194',
-			user: 'u178938601',
-			password: '24B0kB3F4hW5',
+			host: 'xx.xxx.xxx.xxx',
+			user: '---',
+			password: '---',
 			uploadPath: 'public_html/masterica'
 
 		}))
